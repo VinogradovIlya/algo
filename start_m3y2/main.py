307d0f8326483160c2a2ac80a9ae3d1d701ef080
Игорь Воинов <igorvoinov@MacBook-Air-Igor.local>
2024-02-23 14:59:10 +0300

    upd notes adding + functionality for working with notes

--- start_m3y2/main.py
+++ start_m3y2/main.py
@@ -13,16 +13,6 @@ os.chdir(DIR)  # перейти в папку по пути
 app = QApplication([])
 main_win = QWidget()
 
-notes = {
-    'Добро пожаловать': {
-        'text': 'Вводная инструкция',
-        'tags': ['Умные заметки', 'Инструкция']
-    }
-}
-
-with open(DIR/'notes_data.json', 'w', encoding='utf-8') as file:
-    json.dump(notes, file)
-
 
 def show_note() -> None:
     key = list_notes.selectedItems()[0].text()
@@ -32,12 +22,13 @@ def show_note() -> None:
 
 
 def add_note():
-    note_name, ok = QInputDialog(
+    note_name, ok = QInputDialog().getText(
         main_win, 'Добавить заметку', 'Название заметки')
     if ok and note_name != '':
         notes[note_name] = {'text': '', 'tags': []}
         list_notes.addItem(note_name)
         list_tags.addItems(notes[note_name]['tags'])
+
 
 def del_note():
     if list_notes.selectedItems():
@@ -47,9 +38,34 @@ def del_note():
         list_tags.clear()
         field_note.clear()
         list_notes.addItems(notes)
-        with open('notes_data.json', 'w'):
-            ...
-    ...
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys=True,
+                      ensure_ascii=False, indent=4)
+    else:
+        warning = QMessageBox()
+        warning.setText('Выберите заметку для удаления')
+        warning.exec()
+
+
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]['text'] = field_note.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys=True,
+                      ensure_ascii=False, indent=4)
+        done()
+    else:
+        warning = QMessageBox()
+        warning.setText('Выберите заметку для сохранения')
+        warning.exec()
+
+
+def done():
+    done = QMessageBox()
+    done.setText('Выполнено')
+    done.exec()
+
 
 main_h_layout = QHBoxLayout()
 add_left_layout = QVBoxLayout()
@@ -60,17 +76,21 @@ main_h_layout.addLayout(add_right_layout)
 main_win.setLayout(main_h_layout)
 
 field_note = QTextEdit()
-field_note.setText('Введите текст заметки')
 add_left_layout.addWidget(field_note)
 
 label_notes = QLabel('Список заметок')
 list_notes = QListWidget()
 add_h1_line = QHBoxLayout()
+
 button_create_notes = QPushButton('Создать заметку')
+button_create_notes.clicked.connect(add_note)
 button_remove_notes = QPushButton('Удалить заметку')
+button_remove_notes.clicked.connect(del_note)
+button_save_notes = QPushButton('Сохранить заметку')
+button_save_notes.clicked.connect(save_note)
+
 add_h1_line.addWidget(button_create_notes)
 add_h1_line.addWidget(button_remove_notes)
-button_save_notes = QPushButton('Сохранить заметку')
 
 add_right_layout.addWidget(label_notes)
 add_right_layout.addWidget(list_notes)
@@ -96,9 +116,23 @@ add_right_layout.addWidget(button_save_tags)
 
 list_notes.itemClicked.connect(show_note)
 
-with open("notes_data.json", "r", encoding='utf-8') as file:
-    notes = json.load(file)
-list_notes.addItems(notes)
+try:
+    with open("notes_data.json", "r", encoding='utf-8') as file:
+        notes = json.load(file)
+    list_notes.addItems(notes)
+except FileNotFoundError:
+    notes = {
+        'Добро пожаловать': {
+            'text': 'Вводная инструкция',
+            'tags': ['Умные заметки', 'Инструкция']
+        }
+    }
+    with open("notes_data.json", "r", encoding='utf-8') as file:
+        notes = json.load(file)
+    list_notes.addItems(notes)
+
+    with open(DIR/'notes_data.json', 'w', encoding='utf-8') as file:
+        json.dump(notes, file, sort_keys=True, ensure_ascii=False, indent=4)
 
 
 main_win.show()
