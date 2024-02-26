fc98b193863b5766df2b2bbf570bb6c9a22c6a46
Игорь Воинов <igorvoinov@MacBook-Air-Igor.local>
2024-02-26 19:36:45 +0300

    create m6y6

--- pro2_m6y6/quiz.py
+++ pro2_m6y6/quiz.py
@@ -1,31 +1,34 @@
 from flask import Flask, session, request, redirect, url_for
 from db_scripts import get_question_after, get_quises
- 
+
+
 def start_quis(quiz_id):
     '''создаёт нужные значения в словаре session'''
     session['quiz'] = quiz_id
     session['last_question'] = 0
- 
+
+
 def end_quiz():
     session.clear()
- 
+
+
 def quiz_form():
     ''' функция получает список викторин из базы и формирует форму с выпадающим списком'''
     html_beg = '''<html><body><h2>Выберите викторину:</h2><form method="post" action="index"><select name="quiz">'''
     frm_submit = '''<p><input type="submit" value="Выбрать"> </p>'''
-
 
     html_end = '''</select>''' + frm_submit + '''</form></body></html>'''
     options = ''' '''
     q_list = get_quises()
     for id, name in q_list:
         option_line = ('''<option value="''' +
-                        str(id) + '''">''' +
-                        str(name) + '''</option>
+                       str(id) + '''">''' +
+                       str(name) + '''</option>
                       ''')
         options = options + option_line
     return html_beg + options + html_end
-       
+
+
 def index():
     ''' Первая страница: если пришли запросом GET, то выбрать викторину, 
     если POST - то запомнить id викторины и отправлять на вопросы'''
@@ -35,13 +38,14 @@ def index():
         return quiz_form()
     else:
         # получили дополнительные данные в запросе! Используем их:
-        quest_id = request.form.get('quiz') # выбранный номер викторины 
+        quest_id = request.form.get('quiz')  # выбранный номер викторины
         start_quis(quest_id)
         return redirect(url_for('test'))
- 
+
+
 def test():
     '''возвращает страницу вопроса'''
-    # что если пользователь без выбора викторины пошел сразу на адрес '/test'? 
+    # что если пользователь без выбора викторины пошел сразу на адрес '/test'?
     if not ('quiz' in session) or int(session['quiz']) < 0:
         return redirect(url_for('index'))
     else:
@@ -53,20 +57,24 @@ def test():
             session['last_question'] = result[0]
             # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
             return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
- 
+
+
 def result():
     end_quiz()
     return "that's all folks!"
- 
+
+
 # Создаём объект веб-приложения:
-app = Flask(__name__)  
+app = Flask(__name__)
 app.add_url_rule('/', 'index', index)   # создаёт правило для URL '/'
-app.add_url_rule('/index', 'index', index, methods=['post', 'get']) # правило для '/index' 
-app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
-app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
+app.add_url_rule('/index', 'index', index,
+                 methods=['post', 'get'])  # правило для '/index'
+app.add_url_rule('/test', 'test', test)  # создаёт правило для URL '/test'
+# создаёт правило для URL '/test'
+app.add_url_rule('/result', 'result', result)
 # Устанавливаем ключ шифрования:
 app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
- 
+
 if __name__ == "__main__":
     # Запускаем веб-сервер:
     app.run()
